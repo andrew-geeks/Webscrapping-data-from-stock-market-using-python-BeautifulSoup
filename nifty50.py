3cdef7b64984ce0836c1245e8d7245213d0850f9
Andrew George Issac <andrewgeorgeissac@gmail.com>
2020-05-20 11:47:49 +0400

    Nifty50 data code

--- nifty50.py
+++ nifty50.py
@@ -13,4 +13,17 @@ print('change value: ',float(value1)) #change value
 value2=round(((float(value1))/float(value))*100,2) #change %
 print('Change%: ',str(value2)+'%')
 
+value3=soup.find(class_="low").get_text()
+real=value3.split(',')
+value3=''
+for i in range(len(real)):
+            value3=value3+real[i]
+print('Lowest Value: ',float(value3)) #lowest value
+
+value4=soup.find(class_="high").get_text()
+real1=value4.split(',')
+value4=''
+for i in range(len(real1)):
+            value4=value4+real[i]
+print('Highest Value: ',float(value4)) #highest value
 #data from moneycontrol.com(NSE India)
